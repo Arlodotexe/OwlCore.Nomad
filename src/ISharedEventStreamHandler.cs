739f3ab272366114435e2354a12684b3bb223af6
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-09-14 16:34:19 -0500

    Release 0.8.1. See release notes or expand full commit message.
    [Fixes]
    Inherited fixes from OwlCore.ComponentModel 0.9.1.
    
    [Improvements]
    Minor documentation improvements.
    Removed unecessary dependency on OwlCore.Extensions.

--- src/ISharedEventStreamHandler.cs
+++ src/ISharedEventStreamHandler.cs
@@ -9,7 +9,6 @@ namespace OwlCore.Nomad;
 /// <typeparam name="TContentPointer">An immutable pointer to data in the event stream.</typeparam>
 /// <typeparam name="TEventStream">The type used to specify multiple event stream sources during replay.</typeparam>
 /// <typeparam name="TEventStreamEntry">The type for the resolved event stream entries.</typeparam>
-/// <typeparam name="TEventEntryUpdate">The type stored within each event stream's <see cref="EventStreamEntry{TContentPointer}.Content"/>.</typeparam>
 /// <typeparam name="TListeningHandlers">The type of listener/handler instances for this event stream.</typeparam>
 public interface ISharedEventStreamHandler<TContentPointer, TEventStream, TEventStreamEntry, TListeningHandlers> : IEventStreamHandler<TContentPointer, TEventStream, TEventStreamEntry>
     where TEventStream : EventStream<TContentPointer>
@@ -17,7 +16,7 @@ public interface ISharedEventStreamHandler<TContentPointer, TEventStream, TEvent
     where TListeningHandlers : ISharedEventStreamHandler<TContentPointer, TEventStream, TEventStreamEntry, TListeningHandlers>
 {
     /// <summary>
-    /// A shared collection of all available event streams that should participate in playback of events using their respective <see cref="IEventStreamHandler{TContentPointer, TEventStream, TEventStreamEntry, TEventEntryUpdate}.AdvanceEventStreamAsync"/>. 
+    /// A shared collection of all available event streams that should participate in playback of events using their respective <see cref="IEventStreamHandler{TContentPointer, TEventStream, TEventStreamEntry}.AdvanceEventStreamAsync"/>. 
     /// </summary>
     /// <remarks>
     /// Add the current <see cref="ISharedEventStreamHandler{TEventEntryContent,TEventSources, TEventHandlerEntry, TListeningHandlers}"/> instance to your list. This list should be trickled-down to all instances of <see cref="ISharedEventStreamHandler{TEventEntryContent,TEventSources, TEventHandlerEntry, TListeningHandlers}"/> throughout the application that wishes to receive events.
@@ -31,7 +30,6 @@ public interface ISharedEventStreamHandler<TContentPointer, TEventStream, TEvent
 /// <typeparam name="TContentPointer">An immutable pointer to data in the event stream.</typeparam>
 /// <typeparam name="TEventStream">The type used to specify multiple event stream sources during replay.</typeparam>
 /// <typeparam name="TEventStreamEntry">The type for the resolved event stream entries.</typeparam>
-/// <typeparam name="TEventEntryUpdate">The type stored within each event stream's <see cref="EventStreamEntry{TContentPointer}.Content"/>.</typeparam>
 public interface ISharedEventStreamHandler<TContentPointer, TEventStream, TEventStreamEntry> : ISharedEventStreamHandler<TContentPointer, TEventStream, TEventStreamEntry, ISharedEventStreamHandler<TContentPointer, TEventStream, TEventStreamEntry>>
     where TEventStream : EventStream<TContentPointer>
     where TEventStreamEntry : EventStreamEntry<TContentPointer>
